eda722a4386861803c8075675d3daed9b5545ba1
randyviola <randyh0329@gmail.com>
2016-02-17 16:29:47 +0800

    change to rest client and fix logger

--- lib/embulk/output/bigobject.rb
+++ lib/embulk/output/bigobject.rb
@@ -2,8 +2,8 @@ module Embulk
   module Output
 
     class Bigobject < OutputPlugin
-      require 'logger'
-      require 'net/http'
+
+      require 'rest-client'
       require 'json'
 
       Plugin.register_output("bigobject", self)
@@ -15,11 +15,27 @@ module Embulk
           "port" => config.param("port", :integer, default: 9090),  # integer, optional
           "table" => config.param("table", :string),        # string, required
         }
-        @@log = Logger.new(STDOUT)
-        @@tblexist = false
+        
+        @@bo_url = "http://#{task['host']}:#{task['port']}/cmd".freeze
         @@ttlcounter = 0
+        
         # resumable output:
         # resume(task, schema, count, &control)
+
+        # Create Table if not exists
+        response = send_stmt2borest("desc #{task['table']}") # check table
+        if response["Status"] == 0 then # table exist
+              Embulk.logger.debug { "#{response}" }
+        elsif response["Status"] == -11 then # table not exist
+          response = send_stmt2borest("#{create_botable_stmt("#{task['table']}",schema)}")
+            if response["Status"] != 0 then 
+              Embulk.logger.error { "#{response}" }
+              raise "Create table #{task['table']} in BigObject Failed"
+            end
+        else # should not be here
+          Embulk.logger.error { "#{response}" }
+          raise "Please check table #{task['table']} in BigObject First"  
+        end
 
         # non-resumable output:
         task_reports = yield(task)
@@ -36,24 +52,11 @@ module Embulk
 
       def init
         # initialization code:
-
-        @host = task["host"]
-        @port = task["port"]
         @table = task["table"]
-
-        @uri = URI.parse("http://#@host:#@port/cmd")
-        @http = Net::HTTP.new(@uri.host, @uri.port)
-        @counter = 0
-        
-        if !istbexist(@table)
-          if !createtbl(@table)
-            abort
-          end
-        end
+        @counter = 0 
       end
 
       def close
-        @http.finish
       end
 
       def add(page)
@@ -64,17 +67,17 @@ module Embulk
         
         page.each do |records|
           values = []
-          records.each do |row| values << "#{row}".to_json end
+          records.each do |row| values << row.to_json end
           data.push("(#{values.join(",")})")
         end
         @counter += data.length
         @@ttlcounter += data.length
 
-        rsp = send_stmt("INSERT INTO #@table VALUES #{data.join(",")}")
+        rsp = self.class.send_stmt2borest("INSERT INTO #@table VALUES #{data.join(",")}")
         if rsp["Status"] == 0 then
-          @@log.info "add #{data.length} to BigObject - #@counter | #@@ttlcounter"
+           Embulk.logger.debug { "add #{data.length} to BigObject - #@counter | #@@ttlcounter" }
         else
-          @@log.error "add #{data.length} to BigObject failed!!"
+           Embulk.logger.error { "add #{data.length} to BigObject failed!!" }
           abort
         end
 
@@ -88,64 +91,45 @@ module Embulk
       end
 
       def commit
-        task_report = {}
+        task_report = {
+          "records" => @counter
+        }
         return task_report
       end
 
-private
-        def send_stmt(stmt)
-          begin
-            params = {"stmt" => "#{stmt}"}
-            json_headers = {"Content-Type" => "application/json", "Accept" => "application/json"}
-            response = @http.post(@uri.path, params.to_json, json_headers)
-            return JSON.parse(response.body)
-          rescue Exception => e
-            @@log.error e
-            abort
-          end
-          
+      def self.send_stmt2borest(stmt)
+        begin
+          response = RestClient.post @@bo_url, { "Stmt" => "#{stmt}" }.to_json, :content_type => :json, :accept => :json
+          JSON.parse(response.body)
+        rescue Exception => e
+          Embulk.logger.error { e }
         end
-  
-        def istbexist(tbl)
-          if !@@tblexist then
-            stmt = "desc #{tbl}"
-            rsp = send_stmt(stmt)
-            rsp["Status"] == 0 ? @@tblexist = true : false
+      end
+
+      def self.create_botable_stmt(tbl,schema)
+        bo_table_schema = schema.map {|column| "#{column.name} #{to_bigobject_column_type(column.type.to_s, column.format.to_s)}" }.join(',')
+        "CREATE TABLE #{tbl} (#{bo_table_schema})"
+      end
+
+      def self.to_bigobject_column_type(type, format)
+        case type
+        when 'long'
+          botype = :INT64
+        when 'boolean'
+          botype = :INT8
+        when 'string'
+          botype = :STRING
+        when 'double'
+          botype = :DOUBLE
+        when 'timestamp'
+          if format.include? "%H" 
+            botype = :DATETIME32
+          else
+            botype = :DATE32
           end
-          return @@tblexist
         end
-  
-        def createtbl(tbl)
-          rsp = send_stmt("CREATE TABLE #{tbl}  (#{change2botype(schema).join(",")})")
-          rsp["Status"] == 0 ? @@tblexist = true : false
-          return @@tblexist
-        end
-  
-        def change2botype(schema)
-          colntype = Array.new
-          schema.each do |s|
-            botype = ""
-            case s[2]
-              when /long/
-                botype = :INT64
-              when /boolean/
-                botype = :INT8
-              when /string/
-                botype = :STRING
-              when /double/
-                botype = :DOUBLE
-              when /timestamp/
-                if s[3].include? "%H" 
-                  botype = :DATETIME32
-                else
-                  botype = :DATE32
-                end
-            end
-            
-            colntype << "'#{s[1]}' #{botype}"
-          end
-          return colntype
-        end 
+        botype
+      end
 
     end
 
